561f740c115e62e3a7ea3e2c6457c280bded280f
c01d-br0th3r <60615552+c01d-br0th3r@users.noreply.github.com>
2021-03-28 14:56:50 +0900

    feat: setting expo env and add default todos function

--- App.js
+++ App.js
@@ -1,21 +1,60 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from "react";
+import {
+  StyleSheet,
+  Text,
+  View,
+  TextInput,
+  Button,
+  SafeAreaView,
+} from "react-native";
 
 export default function App() {
+  const [enteredGoal, setEnteredGoal] = useState("");
+  const [courseGoals, setCourseGoals] = useState([]);
+
+  const handleChange = (enteredText) => {
+    setEnteredGoal(enteredText);
+  };
+
+  const handlePress = () => {
+    setCourseGoals((currentGoals) => [...currentGoals, enteredGoal]);
+  };
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <SafeAreaView>
+        <View style={styles.topSection}>
+          <TextInput
+            placeholder="Input your goal"
+            style={styles.inputField}
+            onChangeText={handleChange}
+            value={enteredGoal}
+          />
+          <Button title="ADD" onPress={handlePress} />
+        </View>
+        <View>
+          {courseGoals.map((goal) => (
+            <Text>{goal}</Text>
+          ))}
+        </View>
+      </SafeAreaView>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    paddingHorizontal: 20,
+  },
+  topSection: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+  },
+  inputField: {
+    width: "80%",
+    borderBottomColor: "black",
+    borderBottomWidth: 1,
+    padding: 10,
   },
 });
